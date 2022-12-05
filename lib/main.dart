cb7981423d6ab88c8859ed7e5920f06aa2f7d506
M Madhav Reddy <mmrsjce@gmail.com>
2022-12-05 08:17:00 +0530

    updated all SubjectsTile UI

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:attendance_manager/screens/student_screens/all_subjects_screen.d
 import 'package:attendance_manager/screens/student_screens/login_screen.dart';
 import 'package:attendance_manager/screens/student_screens/subject_calendar_screen.dart';
 import 'package:attendance_manager/screens/teacher_screens/all_classes_screen.dart';
-import 'package:attendance_manager/screens/teacher_screens/single_class_screen.dart';
 import 'package:attendance_manager/screens/teacher_screens/students_list_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -22,7 +21,6 @@ class MyApp extends StatelessWidget {
         SubjectCalendarScreen.routeName: (ctx) => SubjectCalendarScreen(),
         AllSubjectsScreen.routeName: (ctx) => AllSubjectsScreen(),
         AllClassesScreen.routeName:(ctx)=>AllClassesScreen(),
-        SingleClassScreen.routeName:(ctx)=>SingleClassScreen(),
         StudentsListScreen.routeName:(ctx)=>StudentsListScreen(),
       },
     );
--- lib/widgets/all_classes_tile.dart
+++ lib/widgets/all_classes_tile.dart
@@ -1,4 +1,4 @@
-import 'package:attendance_manager/screens/teacher_screens/single_class_screen.dart';
+import 'package:attendance_manager/screens/teacher_screens/students_list_screen.dart';
 import 'package:flutter/material.dart';
 
 class ClassTile extends StatelessWidget {
@@ -6,52 +6,125 @@ class ClassTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: InkWell(
-        onTap: (){
-          Navigator.pushNamed(context, SingleClassScreen.routeName);
-        },
+    return InkWell(
+      child: Card(
+        margin: EdgeInsets.all(13),
         child: Container(
+          height: 90,
+          width: double.infinity,
           decoration: BoxDecoration(
-            color: Colors.orange,
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          child: Column(
+              color: Color(0xff93B9ED), borderRadius: BorderRadius.circular(5)),
+          child: Row(
             children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
+              Container(
+                height: double.infinity,
+                decoration: const BoxDecoration(
+                    color: Colors.black54,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(5),
+                        bottomLeft: Radius.circular(5))),
+                child: const RotatedBox(
+                  quarterTurns: 3,
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'CSE:5B', textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white, letterSpacing: 4),
+                      // child: AutoSizeText(
+                      //   subject.getTodayStatus(todayDate),
+                      //   style: TextStyle(color: Colors.white),
+                      //   textAlign: TextAlign.center,
+                      //   maxLines: 1,
+                      //   minFontSize: 6,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Column(
                   children: [
-                    Expanded(
-                      child: Container(
-                        color: Colors.green,
-                        child: Text("5th sem"),
+                    const Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Center(
+                          child: Text(
+                            'Data structures',
+                            style: TextStyle(color: Colors.black, fontSize: 23),
+                          ),
+                        ),
                       ),
                     ),
-                    Expanded(
-                      child: Container(
-                        color: Colors.red,
-                        child: Text("B"),
-                      ),
-                    ),
-                    Expanded(
-                      child: Container(
-                        color: Colors.yellow,
-                        child: Text("CSE"),
-                      ),
-                    ),
+                    Row(
+                      children: [
+                        ActionButton(
+                          color: Colors.green.shade300,
+                          text: 'Attendence',
+                          borderRadius: 0.0,
+                          onPressed: true,
+                        ),
+                        ActionButton(
+                          color: Colors.red.shade300,
+                          text: 'cie',
+                          borderRadius: 5.0,
+                          onPressed: false,
+                        ),
+                      ],
+                    )
                   ],
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  color: Colors.grey,
-                  child: Text("Data Structures"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ActionButton extends StatelessWidget {
+  final Color color;
+  final String text;
+  final double borderRadius;
+  final bool onPressed;
+  const ActionButton(
+      {super.key,
+      required this.color,
+      required this.text,
+      required this.borderRadius,
+      required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Material(
+        child: Ink(
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius:
+                BorderRadius.only(bottomRight: Radius.circular(borderRadius)),
+          ),
+          child: InkWell(
+            onTap: () {
+              if (onPressed) {
+                //Attendance Screen
+                Navigator.of(context).pushNamed(StudentsListScreen.routeName);
+              } else {
+                //Cie Screen
+              }
+            },
+            child: Center(
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Text(
+                  text,
+                  style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 17,
+                      fontFamily: 'OpenSans',
+                      fontWeight: FontWeight.w800),
                 ),
               ),
-            ],
+            ),
           ),
         ),
       ),
