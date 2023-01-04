a035a170953d00b3317cb491e8a910f134ba533c
kartiksureja <97840724+kartiksureja@users.noreply.github.com>
2023-01-04 09:30:24 +0530

    CIE table refactored

--- lib/widgets/subject_tile.dart
+++ lib/widgets/subject_tile.dart
@@ -132,34 +132,7 @@ class _SubjectTileState extends State<SubjectTile> {
                 child: Center(
                     child: Column(
                   children: [
-                    if (_isExpanded)
-                      Container(
-                          width: double.infinity,
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Table(
-                              border: TableBorder.all(
-                                  color: Colors.white60, width: 3),
-                              children: [
-                                TableRow(children: [
-                                  TableData(data: "T1"),
-                                  TableData(data: "E1"),
-                                  TableData(data: "T2"),
-                                  TableData(data: "E2"),
-                                  TableData(data: "T3"),
-                                  TableData(data: "AVG"),
-                                ]),
-                                TableRow(children: [
-                                  TableData(data: "12"),
-                                  TableData(data: "12"),
-                                  TableData(data: "12"),
-                                  TableData(data: "12"),
-                                  TableData(data: "12"),
-                                  TableData(data: "60")
-                                ])
-                              ],
-                            ),
-                          )),
+                    if (_isExpanded) CieTable(),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
@@ -190,6 +163,42 @@ class _SubjectTileState extends State<SubjectTile> {
         ),
       ),
     );
+  }
+}
+
+class CieTable extends StatelessWidget {
+  const CieTable({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Table(
+            border: TableBorder.all(color: Colors.white60, width: 3),
+            children: [
+              TableRow(children: [
+                TableData(data: "T1"),
+                TableData(data: "E1"),
+                TableData(data: "T2"),
+                TableData(data: "E2"),
+                TableData(data: "T3"),
+                TableData(data: "AVG"),
+              ]),
+              TableRow(children: [
+                TableData(data: "12"),
+                TableData(data: "12"),
+                TableData(data: "12"),
+                TableData(data: "12"),
+                TableData(data: "12"),
+                TableData(data: "60")
+              ])
+            ],
+          ),
+        ));
   }
 }
 
