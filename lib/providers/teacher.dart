50e50af6f980dfadb38e026c8978ff242eedbe6c
Shivam Menda <shivammenda2002@gmail.com>
2023-01-04 17:04:32 +0530

    Revert "static data"
    This reverts commit 920f9e7cc9eed23c12c8f636f26510d2ba59c41b.

--- lib/providers/teacher.dart
+++ lib/providers/teacher.dart
@@ -10,31 +10,24 @@ class Teacher {
   Teacher(this.teacherId, this.name, this.assignedClasses);
 }
 
-class TeacherProvider {
-  var responseData;
-  Teacher teacher = Teacher("teacherId", "name", []);
-  void fetchAndSetTeacher() async {
-    SharedPreferences sp = await SharedPreferences.getInstance();
-    String? token = sp.getString('token');
-    var url = Uri.parse(
-        "https://sjce12345.pythonanywhere.com/api/teacher/get-teacher-details/test@sjce.in/");
-    var response = await http.get(url, headers: {
+class TeacherProvider{
+  Teacher teacher=Teacher("teacherId", "name", []);
+  void fetchAndSetTeacher() async{
+    SharedPreferences sp=await SharedPreferences.getInstance();
+    String? token=sp.getString('token');
+    var url=Uri.parse("https://sjce12345.pythonanywhere.com/api/teacher/get-teacher-details/test@sjce.in/");
+    var response=await http.get(url,headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
       'Authorization': 'Bearer 2d7188e0baacce98017bbfff0a63c9253b43dcc5',
     });
-    //print(response.body);
-    responseData = json.decode(response.body);
-    print(responseData['assignedClasses']);
-    //print(responseData);
-    teacher = Teacher(responseData['id'], responseData['name'],
-        responseData['assignedClasses']);
+    print(response.body);
+    var responseData=json.decode(response.body);
+    print(responseData);
+    teacher=Teacher(responseData['id'], responseData['name'],responseData['assignedClasses']);
   }
-
-  List<dynamic> getClasses() {
-    return [
-      {"section": "B5", "course": "Principles of Compilers"},
-      {"section": "C5", "course": "Principles of Compilers"}
-    ];
+  List<dynamic> getClasses(){
+    print(teacher.assignedClasses);
+    return teacher.assignedClasses;
   }
 }
