43a9bd86890acb354d311b244038cfa1333b13ed
Shivam Menda <shivammenda2002@gmail.com>
2023-01-05 20:04:59 +0530

    main.dart fixed with cie screen routing.

--- lib/widgets/all_classes_tile.dart
+++ lib/widgets/all_classes_tile.dart
@@ -1,12 +1,16 @@
+import 'package:attendance_manager/screens/teacher_screens/all_student_cie.dart';
 import 'package:attendance_manager/screens/teacher_screens/students_list_screen.dart';
 import 'package:flutter/material.dart';
 
 class ClassTile extends StatelessWidget {
-
   final String subjectName;
   final String section;
   final String courseId;
-  const ClassTile({super.key, required this.subjectName, required this.section,required this.courseId});
+  const ClassTile(
+      {super.key,
+      required this.subjectName,
+      required this.section,
+      required this.courseId});
 
   @override
   Widget build(BuildContext context) {
@@ -61,8 +65,10 @@ class ClassTile extends StatelessWidget {
                           color: Colors.green.shade300,
                           text: 'Attendence',
                           borderRadius: 0.0,
-                          onPressed: (){
-                            Navigator.of(context).pushNamed(StudentsListScreen.routeName,arguments: [section,courseId]);
+                          onPressed: () {
+                            Navigator.of(context).pushNamed(
+                                StudentsListScreen.routeName,
+                                arguments: [section, courseId]);
                             //Navigator.of(context).push(MaterialPageRoute(builder: (context)=>StudentsListScreen(section:section ,courseId: courseId,)));
                           },
                         ),
@@ -70,8 +76,9 @@ class ClassTile extends StatelessWidget {
                           color: Colors.red.shade300,
                           text: 'cie',
                           borderRadius: 5.0,
-                          onPressed: (){
-                            //TODO:Navigate to cie screen
+                          onPressed: () {
+                            Navigator.of(context)
+                                .pushNamed(CieScreen.routeName);
                           },
                         ),
                       ],
@@ -110,7 +117,7 @@ class ActionButton extends StatelessWidget {
                 BorderRadius.only(bottomRight: Radius.circular(borderRadius)),
           ),
           child: InkWell(
-            onTap:onPressed,
+            onTap: onPressed,
             child: Center(
               child: Padding(
                 padding: EdgeInsets.all(10.0),
